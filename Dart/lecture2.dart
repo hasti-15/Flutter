d9a481a80d6d34d16793c4c19b80baf930b01eb4
hasti-15 <50300501+hasti-15@users.noreply.github.com>
2020-08-08 00:42:05 -0700

    Create lecture2.dart

---
+++ Dart/lecture2.dart
@@ -0,0 +1,12 @@
+void main() {
+  List<int> values = [45,32,56,99,12];
+ 
+  var largest_value = values[0];
+
+  for (var i = 1; i < values.length; i++) {
+    if (values[i] > largest_value) {
+      largest_value = values[i];
+    }
+  }
+  print("Largest value in the list : ${largest_value}");
+}
